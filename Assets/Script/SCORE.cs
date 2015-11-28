46e9a787c2f58b71331e188552681118526accf7
nanaya7896 <nwserika.nanaya@gmail.com>
2015-11-28 15:41:38 +0900

    フラグ数管理関数をスコアの中に作成 これもフラグを獲得したときに呼び出してフラグの個数を増やすようにする

--- Assets/Script/SCORE.cs
+++ Assets/Script/SCORE.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class SCORE : MonoBehaviour {
     //スコアの描画をするクラス
     public GameObject g_Score;
+    public GameObject g_flag;
     private long score;
+    private long flagCount;
 
 	// Use this for initialization
 	void Start () {
         //スコアを初期値0に指定
         score = 0;
+        flagCount = 0;
     }
 	
 	// Update is called once per frame
@@ -18,11 +21,12 @@ public class SCORE : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.C))
         {
             ScoreUp();
+            FlagGet();
         }
-
+        
         //スコア描画処理
         g_Score.GetComponent<TextMesh>().text = "score : " + score;
-
+        g_flag.GetComponent<TextMesh>().text = "Flag : " + flagCount;
 	}
 
     //スコアをアップする処理
@@ -30,5 +34,10 @@ public class SCORE : MonoBehaviour {
         score+=1000;
         return score;
     }
-    
+    //フラグを手に入れたら呼び出す関数
+    public long FlagGet()
+    {
+        flagCount++;
+        return flagCount;
+    }
 }
