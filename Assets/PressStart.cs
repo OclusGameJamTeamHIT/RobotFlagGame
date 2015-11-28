e2e488f56f3968e73480c9cb0c730f9a8b952932
nanaya7896 <nwserika.nanaya@gmail.com>
2015-11-28 14:02:37 +0900

    Title作成

---
+++ Assets/PressStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour {
+    public GameObject press;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        press.GetComponent<TextMesh>().text = "PRESS START";
+	}
+}
---
+++ Assets/TitleText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleText : MonoBehaviour {
+    public GameObject Title;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Title.GetComponent<TextMesh>().text = "RobotFlagGame";
+	}
+}
