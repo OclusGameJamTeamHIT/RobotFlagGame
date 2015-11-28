961cfe2e5315c6b322e07f1d8d8ed827184d375d
nanaya7896 <nwserika.nanaya@gmail.com>
2015-11-28 15:16:08 +0900

    Timeの表示完了

---
+++ Assets/TIME.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TIME : MonoBehaviour {
+    public GameObject time;
+    public float hTime;
+    private int TimeFrame;
+	// Use this for initialization
+	void Start () {
+        hTime = 60.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (TimeFrame > 60)
+        {
+            hTime--;
+            TimeFrame = 0;
+        }
+        TimeFrame++;
+        time.GetComponent<TextMesh>().text = "Time : " + hTime;
+	}
+}
