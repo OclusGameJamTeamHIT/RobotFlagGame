a2029774e5870f758e44fa8f187e43994ab45d3f
nanaya7896 <nanaya@ikeharashouta-no-MacBook-Pro.local>
2015-11-29 04:30:36 +0900

    スコア処理をつけました！

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+	public GameObject SC;
+	// Use this for initialization
+	void Start () {
+		SC = GameObject.Find("Score");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag=="Coin")
+			Destroy(other.gameObject);
+		SCORE sc = SC.GetComponent<SCORE>();
+		sc.ScoreUp();
+		Debug.Log("当たったよ！");
+
+	}
+}
