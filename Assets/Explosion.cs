d1c04ac180a199929cc09eabbb7f5d7a2ea11a9c
nanaya7896 <nanaya@ikeharashouta-no-MacBook-Pro.local>
2015-11-29 13:47:16 +0900

    弾を生成し当たり判定をつけ爆発させた

---
+++ Assets/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	public GameObject Explo;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag=="Bullet")
+		{
+			Instantiate (Explo,transform.position,transform.rotation);
+			Destroy (this);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+	
+	public GameObject Bullet;
+	public GameObject Gun;
+	public GameObject b2;
+	Vector3 BulletDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Gun = GameObject.Find ("assault_mech_LeftArmRocket");
+			b2 = Instantiate (Bullet);
+			Debug.Log ("弾のインスタンスをしました");
+			b2.transform.position =Gun.transform.position;
+			b2.transform.rotation = Gun.transform.rotation;
+			BulletDirection = (this.transform.position - Gun.transform.position).normalized;
+			if (Physics.Raycast (b2.transform.position, BulletDirection, 50.0f)) {
+				b2.GetComponent<Rigidbody> ().velocity = BulletDirection * 50;
+			} else {
+				Debug.Log ("弾を削除します");
+				//Destroy (b2.gameObject);
+			}
+		}
+	}
+}
